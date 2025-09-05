317eedbcffef55109058294d2d8430e09fb9dfba
Ken <59818054+superken-coding@users.noreply.github.com>
2025-09-05 06:24:11 +0000

    add supa

---
+++ netlify/functions/supa.js
@@ -0,0 +1,13 @@
+
+import { supabase } from "..";
+
+export default async () => {
+
+    const { data: pet } = await supabase.from("test").select("*");
+
+    if (!pet) {
+        return new Response(`Where'd that pet go?`, { status: 404 });
+    }
+
+    return new Response(JSON.stringify(pet));
+};
