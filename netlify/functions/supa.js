c83be17f446e0a5b7cb2778e7aff6b6f6e1cbd51
Ken <59818054+superken-coding@users.noreply.github.com>
2025-09-05 07:37:46 +0000

    fix: query

--- netlify/functions/supa.js
+++ netlify/functions/supa.js
@@ -3,10 +3,10 @@ import { supabase } from "../../utils/db";
 
 export default async () => {
 
-    const { data: pet } = await supabase.from("test").select("*");
+    const { data: pet, error } = await supabase.from("test").select("*");
 
     if (!pet) {
-        return new Response(`Where'd that pet go?`, { status: 404 });
+        return new Response(error ? error.message : 'err', { status: 404 });
     }
 
     return new Response(JSON.stringify(pet));
